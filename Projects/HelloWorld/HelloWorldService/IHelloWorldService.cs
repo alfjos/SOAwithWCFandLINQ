55a0cdab4de916a865a114101da8ca7b8f324f3f
Alfredo José Ortiz Garcia <me@alfredortiz.com>
2012-07-22 20:10:08 -0600

    Add Interface
    Added an interface to the project as a service contract.

---
+++ Projects/HelloWorld/HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCFServices
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        String GetMessage(String name);
+    }
+}
